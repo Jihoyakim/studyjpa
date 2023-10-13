a28e98c2725373acea37c96f740ed4f8a986912f
Jihoyakim <138176589+Jihoyakim@users.noreply.github.com>
2023-10-13 17:42:31 +0900

    1013-01

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,9 +16,10 @@ public class JpaMain {
         tx.begin();
 
         try{
-//            Member member = new Member(200L,"member200");
-//            em.persist(member);
-//
+            Member member = new Member();
+            member.setUsername("C");
+            em.persist(member);
+
 //            em.flush();
 
 
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,29 +4,37 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="name")
+    @Column(name = "name")
     private String username;
 
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
+//    private Integer age;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    @Lob
+//    private String description;
 
     public Member(){}
 
-
+    public void setId(Long id) {
+        this.id = id;
     }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
 
